93e67086fe993e92c95cb5f2cc8fa2f785cae165
anaelChardan <anael.chardan@gmail.com>
2014-02-14 11:23:41 +0100

    Code compatible partout

---
+++ Code/purpurudigger/purpurudigger/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  purpurudigger
+//
+//  Created by Ananas-Mac on 14/02/2014.
+//  Copyright (c) 2014 Ananas-Mac. All rights reserved.
+//
+
+#include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    sf::RenderWindow window(sf::VideoMode(640,480, 32), "Test");
+    sf::Sleep(3.0F);
+    return 0;
+}
+
