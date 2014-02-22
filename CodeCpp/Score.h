132cb17320529d0d0d97b3fda20e309180dc625c
anaelChardan <anael.chardan@gmail.com>
2014-02-22 17:11:13 +0100

    Ajout de ce que contient la classe score

--- CodeCpp/Score.h
+++ CodeCpp/Score.h
@@ -2,7 +2,6 @@
 /// @brief Fichier relatif aux scores du Puru Puru Digger.
 /// @author Anaël Chardan
 /// @author Jérémy Damey
-/// @author Osiris Malbranque
 /// @version 0.1
 /// @date 17/02/2014
 
@@ -11,10 +10,22 @@
 #define __purpurudigger__Score__
 
 #include <iostream>
+#include <vector>
 
 /// @class Score
 /// @brief Modélisation des scores du jeu
 class Score {
+    private :
+        int my_current; //Le score courrant
+        std::vector<int> my_success; //Notre tableau contenant le score de tout nos level
+    
+    public :
+        int getCurrent() const; //Connaitre le score courant
+        int getCurrentStep() const; //Connaitre le niveau
+        int getGlobal() const; //Connaitre le score global
+        int addSuccess(); //Ajoute une case
+        int addPoints(); //Ajoute des points à la case courrante
+    
 };
 
 #endif
