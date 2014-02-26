199d6d92276dc74e01c1456bc224c618b6832ece
anaelChardan <anael.chardan@gmail.com>
2014-02-26 10:04:35 +0100

    Implémentation complète de la classe

--- CodeCpp/EmptyCell.cpp
+++ CodeCpp/EmptyCell.cpp
@@ -9,7 +9,81 @@
 #include "EmptyCell.h"
 
 
+
+/*===========================
+ Les Constructeurs
+ =============================*/
+
+EmptyCell::EmptyCell() {
+    my_type = "EmptyCell";
+    my_x = 0;
+    my_y = 0;
+}
+
+EmptyCell::EmptyCell( int x, int y ) {
+    my_type = "EmptyCell";
+    my_x = x;
+    my_y = y;
+}
+
+EmptyCell::EmptyCell(const EmptyCell &b) {
+    my_type = b.my_type;
+    my_x = b.my_x;
+    my_y = b.my_y;
+}
+
+
+/*===========================
+ Le Destructeur
+ =============================*/
+
+EmptyCell::~EmptyCell() {}
+
+
+/*===========================
+ Les méthodes
+ =============================*/
+
 void
 EmptyCell::toString( std::ostream& O) const {
     O << "  ";
+}
+
+/*===========================
+ Pour éviter la classe abstraite
+ =============================*/
+
+int
+EmptyCell::getPoints() const {
+    return -1;
+}
+
+int
+EmptyCell::getValue() const {
+    return -1;
+}
+
+int
+EmptyCell::getLife() const {
+    return -1;
+}
+
+void
+EmptyCell::addLife() { }
+
+void
+EmptyCell::lostLife() { }
+
+/*===========================
+ Les opérateurs
+ =============================*/
+
+EmptyCell&
+EmptyCell::operator=(const EmptyCell &b) {
+    if ( this != &b ){
+        my_type = b.my_type;
+        my_x = b.my_x;
+        my_y = b.my_y;
+    }
+    return *this;
 }
--- CodeCpp/EmptyCell.h
+++ CodeCpp/EmptyCell.h
@@ -14,7 +14,31 @@
 
 class EmptyCell : public CellBase {
     
-    virtual void toString( std::ostream& O) const;
+    public:
+        //Les constructeurs
+    
+        EmptyCell(); //Constructeur par défaut
+        EmptyCell( int x, int y ); //Le constructeur paramétré
+        EmptyCell( const EmptyCell &b); //Constructeur par copie
+    
+        //Le destructeur
+    
+        virtual ~EmptyCell(); //Destructeur
+    
+        //Les méthodes
+        virtual void toString( std::ostream& O) const;
+    
+        //Les opérateurs
+        virtual EmptyCell& operator=(const EmptyCell & b); //Opérateur d'affectation
+    
+    private :
+        //Eviter la classe abstraite
+    
+        virtual int getValue() const;
+        virtual int getPoints() const;
+        virtual int getLife() const;
+        virtual void addLife();
+        virtual void lostLife();
 };
 
 #endif /* defined(__PuruPuruDigger__EmptyCell__) */
