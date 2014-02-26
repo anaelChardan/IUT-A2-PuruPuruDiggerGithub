8c132f86905ba2c8b770ec6a4cd5dca6e55c95ec
anaelChardan <anael.chardan@gmail.com>
2014-02-26 18:27:48 +0100

    Ajout des view

---
+++ CodeCpp/GameView.cpp
@@ -0,0 +1,9 @@
+//
+//  GameView.cpp
+//  PuruPuruDigger
+//
+//  Created by Ananas-Mac on 26/02/2014.
+//
+//
+
+#include "GameView.h"
---
+++ CodeCpp/GameView.h
@@ -0,0 +1,14 @@
+//
+//  GameView.h
+//  PuruPuruDigger
+//
+//  Created by Ananas-Mac on 26/02/2014.
+//
+//
+
+#ifndef __PuruPuruDigger__GameView__
+#define __PuruPuruDigger__GameView__
+
+#include <iostream>
+
+#endif /* defined(__PuruPuruDigger__GameView__) */
