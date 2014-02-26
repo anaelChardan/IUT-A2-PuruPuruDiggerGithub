8baa7dec26abfe7f84f13f29a290c77432bb17f9
anaelChardan <anael.chardan@gmail.com>
2014-02-26 12:54:08 +0100

    Quelques test pour déplacer le digger

--- CodeCpp/main.cpp
+++ CodeCpp/main.cpp
@@ -14,6 +14,7 @@
 #include "CellBase.h"
 #include "Digger.h"
 #include "ValueCell.h"
+#include "EmptyCell.h"
 
 using namespace std;
 
@@ -24,6 +25,7 @@ int main(int argc, const char * argv[])
    // sf::RenderWindow window(sf::VideoMode(640,480, 32), "Test");
     //sf::Sleep(3.0F);
     
+    CellBase* my_digger = new Digger;
     
     vector<vector<CellBase*> > my_tab;
     
@@ -31,22 +33,44 @@ int main(int argc, const char * argv[])
     for( unsigned long i = 0 ; i < 2 ; i++ )
         my_tab[i].resize(2);
     
-    my_tab[0][0] = new Digger;
+    my_tab[0][0] = my_digger;
     my_tab[0][1] = new ValueCell;
     my_tab[1][0] = new ValueCell;
     my_tab[1][1] = new ValueCell;
     
     for ( unsigned long i = 0 ; i < 2; i++ ) {
         for ( unsigned long j = 0 ; j < 2 ; j ++ ) {
-            cout << my_tab[i][j]->getType() << "   " ;
-            if ( my_tab[i][j]->getType() == "ValueCell" )
-                cout << my_tab[i][j]->getValue() << endl;
-            else
-                cout << endl;
+            cout << *my_tab[i][j] ;
         }
+        cout << endl;
     }
     
+    //On tente de déplacer le digger
+    delete my_tab[0][1];
     
+    my_tab[0][1] = my_digger;
+    my_tab[0][0] = new EmptyCell;
+    
+    
+    for ( unsigned long i = 0 ; i < 2; i++ ) {
+        for ( unsigned long j = 0 ; j < 2 ; j ++ ) {
+            cout << *my_tab[i][j] ;
+        }
+        cout << endl;
+    }
+    
+    cout <<  " La vie du digger " << my_tab[0][1]->getLife() << endl;
+    
+    my_tab[0][1]->lostLife();
+    
+    cout <<  " La vie du digger " << my_tab[0][1]->getLife() << endl;
+    cout <<  " La vie d'une case quelconque " << my_tab[0][0]->getLife() << endl;
+    
+    for ( unsigned long i = 0; i < 2; i++ ) {
+        for ( unsigned long j = 0 ; j < 2 ; j++ ) {
+            delete my_tab[i][j];
+        }
+    }
     
     return 0;
 }
