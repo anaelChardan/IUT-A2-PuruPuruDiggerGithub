576122bbfe25ab8d4b52fc71e5bae0b4ac0fa2fb
anaelChardan <anael.chardan@gmail.com>
2014-02-28 19:10:34 +0100

    Ebauche

--- CodeCpp/GameView.h
+++ CodeCpp/GameView.h
@@ -10,5 +10,43 @@
 #define __PuruPuruDigger__GameView__
 
 #include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include "GameModel.h"
+
+class GameView {
+    private :
+        int my_w;
+        int my_h;
+    
+        //sf::RenderWindow * _window;
+        //sf::Font* _font;
+    
+        //sf::Image* _background_image;
+        //sf::Image* _ball_image;
+        //sf::Image* _bat_image;
+    
+    //  GraphicElement _background_graph;
+    //  GraphicElement _bomb_graph;
+    //  GraphicElement _digger_graph;
+    //  GraphicElement _gold_graph;
+    //  GraphicElement _value_graph;
+    
+    //std::map<Movable_Element * const, GraphicElement *> _elementToGraphicElement;
+    
+    //GameModel * _model;
+    
+    public:
+        GameView(int w, int h, int bpp);
+        ~GameView();
+    
+        void setModel(GameModel * model);
+        //void synchronize();
+        //void afficherScore();
+        //void draw();
+        bool treatEvents();
+};
 
 #endif /* defined(__PuruPuruDigger__GameView__) */
