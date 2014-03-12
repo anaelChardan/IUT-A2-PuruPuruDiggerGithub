ddc5a680fc654e54399de8f8f7c430b3eaa65ebf
anaelChardan <anael.chardan@gmail.com>
2014-03-12 12:10:49 +0100

    plop

--- CodeCpp/GameView.cpp
+++ CodeCpp/GameView.cpp
@@ -17,6 +17,63 @@ using namespace std;
 using namespace sf;
 
 
+/* Début de l'écriture des fonctions pour la SFML
+GameView::GameView(int w, int h, int bpp): _w(w), _h(h){
+    //Le style de la fenêtres
+     _window = new RenderWindow(sf::VideoMode(w, h, bpp), "PuruPuruDigger", sf::Style::Close);
+ 
+    La font pour les scores
+     _fontScore=  new Font();
+     _fontTitle = new Font();
+ 
+    my_backgroundImage = new Image();
+ 
+    my_caseImage = new Image();
+ 
+    _DiggerSprite = new Sprite();
+    _ValueSprite = new Sprite();
+    _GoldSprite = new Sprite();
+    _BombSprite = new Sprite();
+ 
+    //On charge la font pour les scores
+    _fontScore->LoadFromFile("scoreFont.ttf");
+ 
+ 
+    //On charge la font pour le titre du jeu
+    _fontTitle->LoadFromFile( "titleFont.ttf" );
+ 
+    //Si les deux images n'ont pas encore réussies à charger
+     if (!my_backgroundImage->LoadFromFile("background.png") || !my_caseImage->LoadFromFile("case.png") {
+        cout << "Error when loading image" << endl;
+        RETURN EXIT_SUCCESS;
+     }
+     else {
+        //On set les sprites de nos images
+        _DiggerSprite->SetImage(my_caseImage);
+        _ValueSprite->SetImage(my_caseImage);
+        _GoldSprite->SetImage(my_caseImage);
+        _BombSprite->SetImage(my_caseImage);
+     }
+ 
+    //On set les rectangles de lecture de chacune de nos cases !
+    _DiggerSprite->SetSubRect
+ 
+}
+ 
+ GameView::~GameView() {
+    delete _Digger_image;
+    delete _Bomb_image;
+    delete _EmptyCell_image;
+    delete
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
 //Fonctions indépendants qui met en couleurs ou non un message selon l'OS utilisé
 std::string
 colorMessage( const char* out , int color ) {
