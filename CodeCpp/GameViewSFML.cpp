442e1f10240fbe5ca5a71db6cd96930bc01a4fa5
anaelChardan <anael.chardan@gmail.com>
2014-03-12 21:27:48 +0100

    Scheme and target into Xcode IDE for SFML Compilation

--- CodeCpp/GameViewSFML.cpp
+++ CodeCpp/GameViewSFML.cpp
@@ -8,47 +8,61 @@
 
 #include "GameViewSFML.h"
 
-/* Début de l'écriture des fonctions pour la SFML
- GameView::GameView(int w, int h, int bpp): _w(w), _h(h){
- //Le style de la fenêtres
- _window = new RenderWindow(sf::VideoMode(w, h, bpp), "PuruPuruDigger", sf::Style::Close);
+using namespace std;
+using namespace sf;
+
+//Building in progress
+
+//Début de l'écriture des fonctions pour la SFML
+ GameView::GameView(int w, int h, int bpp): _w(w), _h(h) {
+     //Le style de la fenêtres
+     _window = new RenderWindow(sf::VideoMode(w, h, bpp), "PuruPuruDigger", sf::Style::Close);
  
- La font pour les scores
- _fontScore=  new Font();
- _fontTitle = new Font();
+     //La font pour les scores
+     my_fontScore=  new Font();
+     my_fontTitle = new Font();
  
- my_backgroundImage = new Image();
+     my_backgroundImage = new Image();
  
- my_caseImage = new Image();
+     my_caseImage = new Image();
+     
+     my_caseImage->CreateMaskFromColor(Color(r, g, b ) );
+     
+     //On affecte les sprite, je décide de ne pas faire de classe GraphicElement, que je n'ai pas forcément bien compris...
  
- _DiggerSprite = new Sprite();
- _ValueSprite = new Sprite();
- _GoldSprite = new Sprite();
- _BombSprite = new Sprite();
+     my_DiggerSprite = new Sprite();
+     _ValueSprite = new Sprite();
+     _GoldSprite = new Sprite();
+     _BombSprite = new Sprite();
+     
+     //On affecte les sprites de fond
+     my_backgroundGraph = new Sprite();
  
- //On charge la font pour les scores
- _fontScore->LoadFromFile("scoreFont.ttf");
+     //On charge la font pour les scores
+     _fontScore->LoadFromFile("scoreFont.ttf");
  
  
- //On charge la font pour le titre du jeu
- _fontTitle->LoadFromFile( "titleFont.ttf" );
+     //On charge la font pour le titre du jeu
+     _fontTitle->LoadFromFile( "titleFont.ttf" );
  
- //Si les deux images n'ont pas encore réussies à charger
- if (!my_backgroundImage->LoadFromFile("background.png") || !my_caseImage->LoadFromFile("case.png") {
- cout << "Error when loading image" << endl;
- RETURN EXIT_SUCCESS;
- }
- else {
- //On set les sprites de nos images
- _DiggerSprite->SetImage(my_caseImage);
- _ValueSprite->SetImage(my_caseImage);
- _GoldSprite->SetImage(my_caseImage);
- _BombSprite->SetImage(my_caseImage);
- }
+     //Si les deux images n'ont pas encore réussies à charger
+     if (!my_backgroundImage->LoadFromFile("background.png") || !my_caseImage->LoadFromFile("case.png") {
+        cout << "Error when loading image" << endl;
+         RETURN EXIT_SUCCESS;
+     } else {
+         //On set les sprites de nos images
+         my_DiggerSprite->SetImage(my_caseImage);
+         my_ValueSprite->SetImage(my_caseImage);
+         my_GoldSprite->SetImage(my_caseImage);
+         my_BombSprite->SetImage(my_caseImage);
+     }
  
- //On set les rectangles de lecture de chacune de nos cases !
- _DiggerSprite->SetSubRect
- 
+    //On set les rectangles de lecture de chacune de nos cases !
+    my_DiggerSprite->SetSubRect(IntRect();
+    my_ValueSprite->SetSubRect(IntRect();
+    my_GoldSprite->SetSubRect(IntRect();
+
+                            
  }
  
  GameView::~GameView() {
@@ -58,9 +72,3 @@
  delete
  }
  
- 
- 
- 
- 
- 
- */
