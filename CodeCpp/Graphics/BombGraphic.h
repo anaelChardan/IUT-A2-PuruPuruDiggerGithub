1822871b5a4df10f81dd575c10c55a5d42645cd4
anaelChardan <anael.chardan@gmail.com>
2014-05-19 21:37:44 +0200

    PLop

--- CodeCpp/Graphics/BombGraphic.h
+++ CodeCpp/Graphics/BombGraphic.h
@@ -3,12 +3,12 @@
 
 #include <iostream>
 #include "CellBaseGraphic.h"
-
 #include "GraphicElement.h"
 
 class BombGraphic : public CellBaseGraphic {
     public :
-    virtual ~BombGraphic();
-    virtual void setImageToSprite();
+        virtual ~BombGraphic();
+        virtual void setImageToSprite();
 };
+
 #endif /* defined(__PuruPuruDigger__BombGraphic__) */
